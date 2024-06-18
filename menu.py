8de54e3564b3d25c83327bcb4dc84c0b59afeb9e
Lukas-ren <lu.duranh@duocuc.cl>
2024-06-18 12:44:14 -0400

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,70 @@
+from os import system
+lista_trabajador = []
+def menu_principal():
+
+    opciones = {
+        '1': ('Registro de Trabajador', reg_trabajador),
+        '2': ('Listado de trabajadores', lis_trabajador),
+        '3': ('Imprimir planilla de sueldos', imp_trabajador),
+        '4': ('Salir', salir)
+    }
+
+    generar_menu(opciones, '4')
+
+def generar_menu(opciones, opcion_salida):
+    opcion = None
+    while opcion != opcion_salida:
+        system("cls")
+        mostrar_menu(opciones)
+        opcion = leer_opcion(opciones)
+        ejecutar_opcion(opcion, opciones)
+        print() # se imprime una línea en blanco para clarificar la salida por pantalla
+
+def mostrar_menu(opciones):
+    print('Seleccione una opción:')
+    for clave in sorted(opciones):
+        print(f' {clave}) {opciones[clave][0]}')
+
+def leer_opcion(opciones):
+    while (a := input('Opción: ')) not in opciones:
+        print('Opción incorrecta, vuelva a intentarlo.')
+    return a
+
+def ejecutar_opcion(opcion, opciones):
+    opciones[opcion][1]()
+
+
+def reg_trabajador():
+    print('Has elegido la opción 1')
+    nombre = input("Ingrese el nombre del trabajador ")
+    cargo = input("Ingrese el cargo del trabajador ")
+    sueldo_bruto = int(input("Ingrese el sueldo bruto del trabajador "))
+    desc_salud = int(round(sueldo_bruto*7/100.0))
+    desc_afp = int(round(sueldo_bruto*12/100.0))
+    liquido = sueldo_bruto - desc_afp - desc_salud
+    lista_trabajador.append({
+                    "nombres": nombre,
+                    "cargo": cargo,
+                    "sueldo_bruto": sueldo_bruto,
+                    "desc_salud": desc_salud,
+                    "desc_afp": desc_afp,
+                    "liquido": liquido
+                })
+
+def lis_trabajador():
+    print('Has elegido la opción 2')
+    system("cls")
+    print(f"Nombres\t       Cargo\t     Sueldo_Bruto\t  Desc_Salud\t    Desc_AFP\t  Liquido\t")
+    for trabajador in lista_trabajador:
+        print(f"{trabajador['nombres']}\t   {trabajador['cargo']}\t   {trabajador['sueldo_bruto']}\t    {trabajador['desc_salud']}\t   {trabajador['desc_afp']}\t    {trabajador['liquido']}")
+
+
+def imp_trabajador():
+    print('Has elegido la opción 3')
+
+
+def salir():
+    print('Saliendo')
+
+
+menu_principal()
